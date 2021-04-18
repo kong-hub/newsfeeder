10ae7d5d53fcbb528a9cffd52c535ef57396c766
Sorawit Trutsat <sorawit.trutsat@gmail.com>
2021-04-18 14:39:04 +0700

    Refactor he function to support pass by value

--- httpd/main.go
+++ httpd/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	r := gin.Default()
 
-	r.GET("/ping", handler.PingGet)
+	r.GET("/ping", handler.PingGet("King Kong"))
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
